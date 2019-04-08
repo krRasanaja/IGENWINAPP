4b8b2bf6354892163442737c8f4a511426ab80b3
KrRasanjana <kr.rasanjana@gmail.com>
2019-04-08 16:01:04 +0530

    Chnge123

--- IGENWINAPP/GlobalData.cs
+++ IGENWINAPP/GlobalData.cs
@@ -9,7 +9,7 @@ namespace IGENWINAPP
 {
     public class GlobalData
     {
-        //LocalConnectionStri
+        //LocalConnectionStr
         public static string ConString = "Data Source=PE1-PC01\\SQLEXPRESS2008;Initial Catalog=DMSIGENDB01;User ID=sa;Password=#compaq123";
     }
 }
